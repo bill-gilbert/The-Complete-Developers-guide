9a9fdc053057265d15849c6c071ebc57b2642e1d
Pavel Glukhov <pavel.glukhov@t-systems.com>
2022-05-12 18:29:17 +0300

    initial commit

--- chapter1/7-executing/fetchjson/index.ts
+++ chapter1/7-executing/fetchjson/index.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then(response => {
+axios.get(url).then((response) => {
   console.log(response.data);
 });
