822b468a7d759c8e8d34f8feb9f5f6609bececd0
Pavel Glukhov <pavel.glukhov@t-systems.com>
2022-05-13 19:49:20 +0300

    [master] added new features

--- chapter1/8-quick-change/fetchjson/index.ts
+++ chapter1/8-quick-change/fetchjson/index.ts
@@ -12,13 +12,17 @@ axios.get(url).then((response) => {
   const todo = response.data as Todo;
   console.log(response.data);
 
-  const id = todo.id;
-  const title = todo.title;
-  const completed = todo.completed;
+  const id = todo.id!;
+  const title = todo.title!;
+  const completed = todo.completed!;
 
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
     The Todo with ID: ${id}
     Has a title of: ${title}
     Is it finished? ${completed}
   `);
-});
+};
